e9919d1923848bb2a130f99fa01dd67327c26983
kamocyc <ktgk.chr66g@gmail.com>
2020-07-31 05:08:02 +0000

    env

---
+++ src/server/secure_token.ts
@@ -0,0 +1,10 @@
+// Switch this? (need to change site URL)
+export const consumerKey = process.env.CONSUMER_KEY;
+export const consumerSecret = process.env.CONSUMER_SECRET;
+
+// Switch this!!!
+//export const siteUrl = 'http://127.0.0.1:8080/';
+export const siteUrl = process.env.SITE_URL;
+
+export const APP_HASH_KEY = process.env.APP_HASH_KEY;
+export const APP_ENC_KEY = process.env.APP_ENC_KEY;
