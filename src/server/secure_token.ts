ea76fd31064f032149ab1c6e47d287d8eccba918
kamocyc <ktgk.chr66g@gmail.com>
2020-07-31 05:16:35 +0000

    modify

--- src/server/secure_token.ts
+++ src/server/secure_token.ts
@@ -1,10 +1,10 @@
 // Switch this? (need to change site URL)
-export const consumerKey = process.env.CONSUMER_KEY;
-export const consumerSecret = process.env.CONSUMER_SECRET;
+export const consumerKey = process.env.CONSUMER_KEY as string;
+export const consumerSecret = process.env.CONSUMER_SECRET as string;
 
 // Switch this!!!
 //export const siteUrl = 'http://127.0.0.1:8080/';
-export const siteUrl = process.env.SITE_URL;
+export const siteUrl = process.env.SITE_URL as string;
 
-export const APP_HASH_KEY = process.env.APP_HASH_KEY;
-export const APP_ENC_KEY = process.env.APP_ENC_KEY;
+export const APP_HASH_KEY = process.env.APP_HASH_KEY as string;
+export const APP_ENC_KEY = process.env.APP_ENC_KEY as string;
